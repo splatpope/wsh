5e4868de049490418bfc4eeb58b855d609149b96
a2p1k02 <35633190+a2p1k02@users.noreply.github.com>
2021-03-01 16:39:32 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,9 @@ void help() {
     std::cout << "This shell rewrite to a2p1k02" << std::endl;
 }
 
-void cd(std::string line, std::string args) {
-    char* home = strcat(home_path, username);
-    if (line == "cd" || line == "cd ") {
-        chdir(home);
-    } else if (line == "cd " || args != "") {
-        chdir(args.c_str());
-    }
-}
-
-int main()
-{
+int main() {
     std::string line;
-    std::string args;
-    //for (int i = 3; i <= line.length(); i++) {
-    //    args.push_back(line[i]);
-    //}
+    std::string args = line.substr(3, line.find(" "));
     while(true) {
         gethostname(hostname, 1024);
         std::cout << username << "@" << hostname << " ~ $ ";
@@ -38,11 +25,6 @@ int main()
             break;
         } else if(line == "help") {
             help();
-        } else if (line == "cd" || args != "") {
-            for (int i = 3; i < line.length(); i++) {
-                args.push_back(line[i]);
-            }
-            cd(line, args);
         } else {
             std::system(line.c_str());
         }
