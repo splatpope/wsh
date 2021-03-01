733cd54653a5ff6e94c976f1661a7f40725b590c
a2p1k02 <35633190+a2p1k02@users.noreply.github.com>
2021-03-01 15:29:42 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,50 @@
 #include <iostream>
+#include <string>
 #include <cstring>
 #include <unistd.h>
 
+char home_path[] = "/home/";
+
 char command[100];
 char hostname[1024];
+char* username = getlogin();
 
 void help() {
     std::cout << "This shell rewrite to a2p1k02" << std::endl;
 }
 
-void cd(std::string line) {
-    std::strcpy(command, line.c_str());
-    chdir(command);
+void cd(std::string line, std::string args) {
+    char* home = strcat(home_path, username);
+    if (line == "cd" || line == "cd ") {
+        chdir(home);
+    } else if (line == "cd " || args != "") {
+        chdir(args.c_str());
+    }
 }
 
 int main()
 {
     std::string line;
+    std::string args;
+    //for (int i = 3; i <= line.length(); i++) {
+    //    args.push_back(line[i]);
+    //}
     while(true) {
         gethostname(hostname, 1024);
-        std::cout << getlogin() << "@" << hostname << " ~ $ ";
+        std::cout << username << "@" << hostname << " ~ $ ";
         std::cin >> line;
         if (line == "exit") {
             return 1;
             break;
         } else if(line == "help") {
             help();
-        } else if (line == "cd") {
-            cd(line);
+        } else if (line == "cd" || args != "") {
+            for (int i = 3; i < line.length(); i++) {
+                args.push_back(line[i]);
+            }
+            cd(line, args);
         } else {
-            std::strcpy(command, line.c_str());
-            std::system(command);
+            std::system(line.c_str());
         }
     }
 
